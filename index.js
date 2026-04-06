87dc4854d36dcbf919c4359ef4af1833e1ac5036
Brice-647 <bputhoff@ivytech.edu>
2026-04-06 18:00:32 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import * as fs from 'fs';
+import * as cowsay from 'cowsay';
 
+import { say } from 'cowsay';
+console.log('choo choo!');
+
+const output = say({ text: 'WOOF... WOOF...' });
+console.log(output);
+
+// const words = ['unicorn', 'cupcake', 'rainbow', 'kitten'];
+// const output = words.join('\n');
+// fs.writeFile('words.txt', output, fileWritten);
+
+// function fileWritten() {
+//   console.log('file written!');
+// }
